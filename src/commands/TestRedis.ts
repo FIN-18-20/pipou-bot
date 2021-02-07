8f6d360fa7b2887a1a2ce023559068ff667d8fd7
Valentin Kaelin <kalanehd@gmail.com>
2021-02-07 14:48:17 +0100

    feat: first test to add redis

---
+++ src/commands/TestRedis.ts
@@ -0,0 +1,13 @@
+import Redis from '../services/Redis';
+import { Command } from '../framework';
+
+export default new Command({
+  enabled: true,
+  name: 'redis',
+  description: 'Test redis command',
+  async handle({ message }) {
+    const oldMsg = await Redis.get('yolo');
+    message.channel.send("Contenu de l'ancien message: " + oldMsg);
+    await Redis.set('yolo', message.content);
+  },
+});
---
+++ src/services/Redis.ts
@@ -0,0 +1,29 @@
+import { promisify } from 'util';
+import redis, { RedisClient } from 'redis';
+
+class Redis {
+  private _client: RedisClient | null;
+
+  public get: (key: string) => Promise<unknown>;
+  public set: (key: string, value: string) => Promise<unknown>;
+
+  constructor() {
+    this._client = redis.createClient();
+    this.get = promisify(this._client.get).bind(this._client);
+    this.set = promisify(this._client.set).bind(this._client);
+  }
+
+  /**
+   * Returns the Redis Client instance.
+   * The bot must be started first.
+   */
+  public get client(): RedisClient {
+    if (!this._client) {
+      throw new Error('Redis was not started');
+    }
+
+    return this._client;
+  }
+}
+
+export default new Redis();
