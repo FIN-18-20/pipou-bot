23bad47133003837b7c866de2cd59fddb9d383f8
Lazzzer <lazarpavicevic@protonmail.com>
2021-02-11 22:59:10 +0100

    feat(Homework): add Luxon and finished modify

--- src/commands/Homework.ts
+++ src/commands/Homework.ts
@@ -1,12 +1,15 @@
 import Redis from '../services/Redis';
 import { EmbedFieldData, MessageEmbed, TextChannel } from 'discord.js';
 import { Command } from '../framework'
+import { DateTime, Interval } from 'luxon'
+
+// TODO Lazar: Refactor repeating code
 
 interface Homework {
   id: number;
   module: string;
   description: string;
-  date: Date;
+  date: string;
 }
 
 // TODO Put this function in Redis service ?
@@ -43,38 +46,40 @@ export default new Command({
     }
 
     // ANCHOR ADD command
-    // TODO Pre-condition : validate args || Use Luxon for date manipulation
     if (args[1] === "add" && args.length === 4) {
       const description = args[2];
-      const date = args[3].split('.')
-
+      const date = DateTime.fromFormat(args[3], "dd.MM.yyyy", { locale: "fr" });
       const homework: Homework = {
         id: Date.now(),
         module: channelName,
         description: description,
-        date: new Date(Number(date[2]), Number(date[1]) - 1, Number(date[0])) // Fuck this shit
+        date: date.toISO()
+      }
+
+      if(!date.isValid || date < DateTime.local()) {
+        message.channel.send("Invalid date argument. Correct date synthax is dd.mm.yyyy.");
+        return;
       }
 
       const key = 'hw-' + channelName + '-' + homework.id;
-      const ttl = Math.floor(Math.abs((homework.date.getTime() - Date.now()) / 1000)) + 86400; // + one day
-
+      const ttl = Math.floor(Math.abs(Interval.fromDateTimes(DateTime.local(), date).length('seconds'))) + 86400; // + one day
       await Redis.setex(key, ttl, JSON.stringify(homework));
 
+      console.log("FIRST: " + homework.date);
       const embed = new MessageEmbed()
         .setColor('#fad541')
         .setTitle('Homework added for ' + channelName)
         .setDescription('ID: ' + homework.id)
         .addFields(
-          { name: 'Due for ' + homework.date.toLocaleDateString('fr-FR'), value: homework.description }
+          { name: 'Due for ' + date.toLocaleString(), value: homework.description }
         )
+
       message.channel.send(embed);
       return;
     }
 
     // ANCHOR SHOW command
-    // TODO refactor repeating code with show-all
     if (args[1] === "show" && args.length === 2) {
-
       const keys = await scanKeys('0', 'hw-' + channelName + '-*');
 
       if (keys.length) {
@@ -85,22 +90,18 @@ export default new Command({
 
           if (data != null) {
             const homework: Homework = JSON.parse(data);
-            const date = new Date(homework.date);
+            const name = (index + 1) + '. ' + homework.description
+            const value: string = 'Due for ' + DateTime.fromISO(homework.date).toLocaleString({ locale: "fr" }) + '\n ID: ' + homework.id;
 
-            const name = (index + 1) + '. ' + homework.description;
-            const value: string = 'Due for ' + date.toLocaleDateString('fr-FR') + '\n ID: ' + homework.id;
-
-            embedFields.push({ name: name, value: value, inline: false })
+            embedFields.push({ name: name, value: value, inline: false });
           }
         }
-
         const embed = new MessageEmbed()
           .setColor('#fad541')
           .setTitle('Homeworks for ' + channelName)
           .addFields(...embedFields)
         message.channel.send(embed);
         return;
-
       } else {
         const embed = new MessageEmbed()
           .setColor('#fad541')
@@ -112,7 +113,6 @@ export default new Command({
 
     // ANCHOR SHOW-ALL command
     if (args[1] === "show-all" && args.length === 2) {
-
       const keys = await scanKeys('0', 'hw-*');
 
       if (keys.length) {
@@ -123,22 +123,18 @@ export default new Command({
 
           if (data != null) {
             const homework: Homework = JSON.parse(data);
-            const date = new Date(homework.date);
-
             const name = (index + 1) + '. [' + homework.module + '] ' + homework.description;
-            const value: string = 'Due for ' + date.toLocaleDateString('fr-FR') + '\n ID: ' + homework.id;
+            const value: string = 'Due for ' + DateTime.fromISO(homework.date).toLocaleString({ locale: "fr" }) + '\n ID: ' + homework.id;
 
-            embedFields.push({ name: name, value: value, inline: false })
+            embedFields.push({ name: name, value: value, inline: false });
           }
         }
-
         const embed = new MessageEmbed()
           .setColor('#fad541')
           .setTitle('Homeworks for all courses')
           .addFields(...embedFields)
         message.channel.send(embed);
         return;
-
       } else {
         const embed = new MessageEmbed()
           .setColor('#fad541')
@@ -151,7 +147,7 @@ export default new Command({
     // ANCHOR DELETE command
     if (args[1] == "delete" && args.length === 3) {
       const id = args[2];
-      const keys = await scanKeys('0', 'hw-*-' + id)
+      const keys = await scanKeys('0', 'hw-*-' + id);
       if (keys.length) {
         await Redis.del(keys[0]);
         const embed = new MessageEmbed()
@@ -170,31 +166,39 @@ export default new Command({
     // ANCHOR MODIFY command
     if (args[1] === "modify" && args.length >= 4 && args.length <= 5) {
       const id = args[2];
-      const keys = await scanKeys('0', 'hw-*-' + id)
+      const keys = await scanKeys('0', 'hw-*-' + id);
       if (keys.length) {
         const homework: Homework = JSON.parse(await Redis.get(keys[0]));
-
+        
         if (args.length === 5) {
-          const description = args[3];
-          const date = args[4].split('.')
-          homework.description = description;
-          homework.date = new Date(Number(date[2]), Number(date[1]) - 1, Number(date[0]));
+          homework.description = args[3];
+          homework.date = DateTime.fromFormat(args[4], "dd.MM.yyyy", { locale: "fr" }).toISO();
         }
 
-        // NOTE Meaning one option was skipped
         if (args.length === 4) {
-          // TODO Need to add Luxon to make it easy to identify/validate dates.
+          const date = DateTime.fromFormat(args[3], "dd.MM.yyyy", { locale: "fr" });
+          if (!date.isValid || date < DateTime.local()){
+            // Check if the argument was meant to be the description by checking if it has at least one letter.
+            if (/^.*[a-zA-Z]+.*$/.test(args[3])) {
+              homework.description = args[3];
+            } else {
+              message.channel.send("Invalid arguments. !hw help to see command synthax.");
+              return;
+            }
+          } else {
+            homework.date = date.toISO();
+          }
         }
 
-        const ttl = Math.floor(Math.abs((homework.date.getTime() - Date.now()) / 1000)) + 86400;
+        const ttl = Math.floor(Math.abs(Interval.fromDateTimes(DateTime.local(),  DateTime.fromISO(homework.date)).length('seconds'))) + 86400;
         await Redis.setex(keys[0], ttl, JSON.stringify(homework));
 
         const embed = new MessageEmbed()
           .setColor('#fad541')
-          .setTitle('Homework modified for ' + channelName)
+          .setTitle('Homework modified for ' + homework.module)
           .setDescription('ID: ' + homework.id)
           .addFields(
-            { name: 'Due for ' + homework.date.toLocaleDateString('fr-FR'), value: homework.description }
+            { name: 'Due for ' + DateTime.fromISO(homework.date).toLocaleString({ locale: "fr" }), value: homework.description }
           )
         message.channel.send(embed);
         return;
@@ -214,11 +218,11 @@ export default new Command({
         .setTitle('Homework commands')
         .setDescription('Manage homeworks for a specific HEIG-VD module. \n Alias: [hw, homework]')
         .addFields(
-          { name: 'Add homework', value: '!hw add "Homework description" jj.mm.yyyy' },
+          { name: 'Add homework', value: '!hw add "Homework description" dd.mm.yyyy' },
           { name: 'Show homeworks (channel bound)', value: '!hw show' },
           { name: 'Show all homeworks', value: '!hw show-all' },
           { name: 'Delete homework', value: '!hw delete homework-id' },
-          { name: 'Modify homework (not working yet)', value: '!hw modify homework-id [optional] "New description" [optional] jj.mm.yyyy' },
+          { name: 'Modify homework', value: '!hw modify homework-id [optional] "New description" [optional] dd.mm.yyyy' },
         )
       message.channel.send(embed);
       return;
