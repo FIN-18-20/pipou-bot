e59cc3a922396c3453cc0665de0d6437f39f45a4
Valentin Kaelin <kalanehd@gmail.com>
2021-02-17 15:05:59 +0100

    refactor(rrh): use resolveJsonModule to import json file

--- src/commands/Rentsch.ts
+++ src/commands/Rentsch.ts
@@ -1,20 +1,16 @@
+import Store from '../services/Store';
 import { Command } from '../framework';
+import { MessageEmbed } from 'discord.js';
 
 export default new Command({
   enabled: true,
   name: 'rrh',
   description: 'Tell a truth about life.',
   async handle({ message }) {
+    const random = Math.floor(Math.random() * Store.rrhQuotes.length);
+    const msg = Store.rrhQuotes[random];
+    const embed = new MessageEmbed().setDescription(msg).setFooter('© Rentsch');
 
-    const fs = require('fs');
-
-    fs.readFile("src/data/rrh.json", (err, data) => {
-      if (err) throw err;
-      const pipou = JSON.parse(data);
-      const rdmnum = Math.floor(Math.random() * pipou.length); 
-      const msg = pipou[rdmnum];
-
-      message.channel.send(msg + " --Rentsch");
-    });
+    message.channel.send(embed);
   },
 });
--- src/services/Store.ts
+++ src/services/Store.ts
@@ -1,8 +1,10 @@
 import { musicQueue } from './Music';
+import rrhQuotes from '../data/rrh.json';
 
 const Store = {
   musicQueues: new Map<string, musicQueue>(),
-  inspiroBotQueues: new Map<string, boolean>()
+  inspiroBotQueues: new Map<string, boolean>(),
+  rrhQuotes,
 };
 
 export default Store;
