d065ec6e1caafee29219c51e03cf83fb219aa40e
Valentin Kaelin <kalanehd@gmail.com>
2021-04-30 22:39:26 +0200

    refactor: move data folder outside of src

--- src/services/Store.ts
+++ src/services/Store.ts
@@ -1,14 +1,14 @@
 import fs from 'fs';
 import path from 'path';
 import { musicQueue } from './Music';
-import rrhQuotes from '../data/rrh.json';
+import rrhQuotes from '../../data/rrh.json';
 
-const directory = path.join(__dirname, '../data/sounds');
+const directory = path.join(__dirname, '../../data/sounds');
 const sounds = new Map<string, string>();
 fs.readdirSync(directory).forEach((name) => {
   sounds.set(
     name.substring(0, name.lastIndexOf('.')),
-    path.join(__dirname, '../data/sounds', name),
+    path.join(__dirname, '../../data/sounds', name),
   );
 });
 
